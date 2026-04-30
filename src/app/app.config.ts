a94db01676969d6ad514c6887319a88fb2625e35
Nathan-Sasa <jonathanbikuta05@gmail.com>
2026-04-30 14:24:03 +0100

    first commit: Angular start structure app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,35 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { withInMemoryScrolling } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
 
 import { routes } from './app.routes';
 
+registerLocaleData(localeFr);
+
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+	providers: [
+		provideBrowserGlobalErrorListeners(),
+		provideZoneChangeDetection({ eventCoalescing: true }),
+		provideRouter(
+			routes,
+			withInMemoryScrolling({
+				scrollPositionRestoration: 'top',
+				anchorScrolling: 'enabled',
+			}),
+		),
+		provideHttpClient(),
+		provideAnimationsAsync(),
+		providePrimeNG({
+            theme: {
+                preset: Aura
+            }
+        }),
+		{ provide: LOCALE_ID, useValue: 'fr-FR' },
+	]
 };
---
+++ src/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+    darkMode: 'dark',
+    content: [
+        './src/**/*.{html,ts}',
+    ],
+    plugins: [],
+}
